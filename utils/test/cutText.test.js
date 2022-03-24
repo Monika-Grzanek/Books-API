04b70f6ee5c17d34117a31182233d958d95328ef
Monika Grzanek <monski@o2.pl>
2022-03-24 15:09:21 +0100

    Transfer condition to cutText

--- utils/test/cutText.test.js
+++ utils/test/cutText.test.js
@@ -3,7 +3,6 @@ const expect = require('chai').expect;
 
 describe('CutText', () => {
     it('should return an error if "content" arg is not a string', () => {
-        if(typeof content !== 'string') return 'Error';
         expect(cutText(undefined, 20)).to.equal('Error');
         expect(cutText(12, 20)).to.equal('Error');
         expect(cutText({}, 20)).to.equal('Error');
